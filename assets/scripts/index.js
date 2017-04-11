7c312da380541be4c2df5df5cdbccdb5d851dba1
AlexMollohan <ajmollohan@gmail.com>
2017-04-11 16:26:32 -0400

    added require statement linking to gameEngine in index.js, then began working on UI in index.html before realizing it would be in my best interests to switch to a new branch

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,7 +2,7 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
-const gameEngine = require('./gameEngine')
+const playGame = require('./playGame')
 
 $(() => {
   setAPIOrigin(location, config)
