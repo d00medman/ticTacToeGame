2da7226a2ada0d4c67616025478879cab884a114
AlexMollohan <ajmollohan@gmail.com>
2017-04-11 10:46:14 -0400

    Moved pre-built engine code over to current file directory. Teardown code is duly marked.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,6 +2,7 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const gameEngine = require('./gameEngine')
 
 $(() => {
   setAPIOrigin(location, config)
