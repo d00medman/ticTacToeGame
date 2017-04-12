2a69b3d0d9719e7d9d656b6d39ca746514260b93
AlexMollohan <ajmollohan@gmail.com>
2017-04-12 11:39:52 -0400

    Click handlers for marking board and reseting board in rudimentary manner have been created and implemented. Next step is integrating click handlers with game logic

--- assets/scripts/gameEvents.js
+++ assets/scripts/gameEvents.js
@@ -2,12 +2,30 @@
 
 const engine = require('./playGame.js')
 
+// burndown. When events are integrated with engine, currentPlayer will be pulled from the engine
+let currentPlayer = true
+// burndown
+
 const mark = function (event) {
+  // burndown
   console.log('test')
+  // burndown
+  const whichCell = $(this).attr('data-id')
+  const whichMark = (currentPlayer === true) ? 'X' : 'O'
+  $('#' + whichCell).text(whichMark)
+  currentPlayer = !currentPlayer
+}
+
+const reset = function (event) {
+  // burndown
+  console.log('test')
+  // burndown
+  $('.cell').text('')
 }
 
 const addHandlers = () => {
   $('.cell').on('click', mark)
+  $('.reset').on('click', reset)
 }
 
 module.exports = {
